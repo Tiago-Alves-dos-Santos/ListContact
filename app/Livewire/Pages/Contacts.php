f3bee7cf4b7dba2960712fb011df6326da074afe
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-11-06 11:59:19 -0300

    multi pagination of list users

--- app/Livewire/Pages/Contacts.php
+++ app/Livewire/Pages/Contacts.php
@@ -2,13 +2,26 @@
 
 namespace App\Livewire\Pages;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Livewire\WithPagination;
 
 class Contacts extends Component
 {
-    public $name = '';
+    use WithPagination;
+    private function listContacts():array
+    {
+        $user = request()->user();
+        return [
+            'contacts' => $user->contacts()->paginate(10)->withQueryString(),
+            'user_system' => $user->contactsSystem()->paginate(10, ['*'], 'user_system')->withQueryString()
+        ];
+    }
     public function render()
     {
-        return view('livewire.pages.contacts')->layout('layouts.app');
+        return view('livewire.pages.contacts', [
+            ...$this->listContacts()
+        ])->layout('layouts.app');
     }
 }
--- resources/views/livewire/pages/contacts.blade.php
+++ resources/views/livewire/pages/contacts.blade.php
@@ -8,7 +8,13 @@
 
     <div class="w-full flex justify-end">
         <a href="{{ route('contacts.new') }}" type="button"
-            class="text-white capitalize bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">{{ __('new') }}</a>
+            class="text-white capitalize bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4
+            focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+            {{ __('new') }}
+        </a>
+    </div>
+    <div>
+        <h2>Contatos</h2>
     </div>
     <x-table>
         <x-slot name="thead">
@@ -20,10 +26,44 @@
                     Numero
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Category
+                    <span class="sr-only">Edit</span>
+                </th>
+            </tr>
+        </x-slot>
+        <x-slot name="body">
+            @forelse ($contacts as $value)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ $value->name }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ $value->cellphone }}
+                </td>
+                <td class="px-6 py-4 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            @empty
+
+            @endforelse
+        </x-slot>
+    </x-table>
+    <div>
+        {{ $contacts->links() }}
+    </div>
+
+
+    <div>
+        <h2>Contatos do sistema</h2>
+    </div>
+    <x-table>
+        <x-slot name="thead">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Product name
                 </th>
                 <th scope="col" class="px-6 py-3">
-                    Price
+                    Numero
                 </th>
                 <th scope="col" class="px-6 py-3">
                     <span class="sr-only">Edit</span>
@@ -31,26 +71,26 @@
             </tr>
         </x-slot>
         <x-slot name="body">
+            @forelse ($user_system as $value)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
+                    {{ $value->user->name }}
                 </th>
                 <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
+                    {{ $value->user->cell_phone_number }}
                 </td>
                 <td class="px-6 py-4 text-right">
                     <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                 </td>
             </tr>
+            @empty
+
+            @endforelse
         </x-slot>
     </x-table>
-
+    <div>
+        {{ $user_system->links() }}
+    </div>
 
 </div>
 <script></script>
