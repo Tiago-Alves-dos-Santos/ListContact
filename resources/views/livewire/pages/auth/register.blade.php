d678903b50d14c18af114d3a68227d066e299d8f
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-11-07 16:53:37 -0300

    wire ui password fields

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -73,8 +73,9 @@ new #[Layout('layouts.guest')] class extends Component {
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password"
+            <x-inputs.password wire:model="password" id="password" type="password" name="password"
                 required autocomplete="new-password" />
+
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -83,7 +84,7 @@ new #[Layout('layouts.guest')] class extends Component {
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
+            <x-inputs.password wire:model="password_confirmation" id="password_confirmation"
                 type="password" name="password_confirmation" required autocomplete="new-password" />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
